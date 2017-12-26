803e8033462bcc1d33d33afd21abd59371ea36ae
Web前端--田进营 <tianjinying93@163.com>
2017-12-26 17:03:37 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+/*class Title extends Component{
+    render(){
+        return
+            <div>
+                tian
+            </div>
+
+    }
+};
+ReactDom.Render(<Title/>,document.getElementById('app'));*/
+ReactDom.render(
+    <div>Hello React!</div>, document.getElementById('app'));
+
